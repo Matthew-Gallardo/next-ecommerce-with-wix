696b722bedf987cf014ef771c08dd762e4ca82d6
Matthew-Gallardo <106909575+Matthew-Gallardo@users.noreply.github.com>
2024-05-24 12:13:18 +0800

    search bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link"
 import Menu from "./menu"
 import Image from "next/image"
+import SearchBar from "./search"
+import NavIcon from "./navicons"
 const NavBar = () => {
     return (
       <div className='h-20 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
         {/*mobile*/} 
-      <div className="h-full flex items-center justify-between">
+      <div className="h-full flex items-center justify-between md:hidden">
       <Link href="/">
         <div className="text-2xl tracking-wide"> TaenaMu</div>
 
@@ -13,16 +15,18 @@ const NavBar = () => {
       <Menu/>
       </div>
       {/*bigger mobile*/} 
-      <div className="hidden md:flex items-center justify-between gap-8 h-fll">
+      <div className="hidden md:flex items-center justify-between gap-8 h-fll ">
        {/*Left*/} 
        <div className="w-1/3">
-        <Link href="/">
+        <Link href="/" className="flex items-center gap-3">
             <Image src= "/logo.png" alt ="" width={24} height={24} />
         <div className="text-2xl tracking-wide"> TaenaMu</div>
         </Link>
        </div>
         {/*Right*/} 
-       <div className="w-2/3"></div>
+       <div className="w-2/3 flex items-center justify-between gap-8"></div>
+       <SearchBar/>
+       <NavIcon/>
         
       </div>
       </div>
